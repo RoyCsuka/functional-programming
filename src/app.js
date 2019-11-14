1a84b0b5f39b6dfa9c0c7213b6b8c41e5b23f9c3
Csukiii <roy-csuka@live.nl>
2019-11-14 10:07:54 +0100

    Begin van D3
    - D3 begonnen
    - Public folder meenemen naar Github voor Github pages

--- src/app.js
+++ src/app.js
@@ -128,22 +128,15 @@ function convertYear(item) {
 
         cleanDate.value = parseInt(cleanDate.value)
 
-        // if (cleanDate.value.toString().includes("-") ===  true) {
-        //         var splitLastData = cleanDate.value.split("-")
-        // }
-
-        // console.log(Number(cleanDate.value))
-
-
     })
 
     let newArr = item;
-
     let finalArray = deleteUnformattedData(newArr)
     console.log(finalArray)
     return newArr;
 }
 
+// Props: Coen
 function deleteUnformattedData (array) {
     const finalArray = array.filter(item => {
         if (item.date.value.toString().length === 4) {
@@ -153,4 +146,195 @@ function deleteUnformattedData (array) {
  return finalArray
 }
 
-main()
+let cleanedDataset = main()
+
+console.log(cleanedDataset)
+
+
+
+
+
+
+// D3 code
+// Algemene variabele
+
+const queryLimit = 10000
+const query = `PREFIX wgs84: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+PREFIX gn: <http://www.geonames.org/ontology#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+PREFIX dc: <http://purl.org/dc/elements/1.1/>
+PREFIX dct: <http://purl.org/dc/terms/>
+PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+# let op: geeft aantal van unieke combinaties van ?date en ?landLabel
+SELECT ?landLabel ?lat ?long ?date (COUNT(?cho) AS ?choCount) WHERE {
+   ?cho dct:created ?date;
+        dct:spatial ?plaats .
+ # We willen geen datums die de string [NI] bevatten
+   FILTER (!REGEX(?date, "[NI]")) . #zorgt ervoor dat de string "NI" niet wordt meegenomen
+ # geef het label van het land waar de plaats ligt
+   ?plaats skos:exactMatch/gn:parentCountry ?land .
+   ?land skos:exactMatch/wgs84:lat ?lat .
+   ?land skos:exactMatch/wgs84:long ?long .
+   ?land gn:name ?landLabel .
+} GROUP BY ?date ?landLabel ?lat ?long
+ORDER BY DESC(?choCount) LIMIT ${queryLimit}`
+//Please use your own endpoint when using this
+const endpoint = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-14/sparql"
+const  dataSource = cleanedDataset
+
+makeVisualization()
+
+// Our main function which runs other function to make a visualization
+async function makeVisualization(){
+  //Wait for the promise to resolve with the data
+  let data = await loadData(endpoint, query)
+  console.log("rawData: ", data)
+	data = data.map(cleanData)
+  console.log("cleanedData: ", data)
+	data = transformData(data)
+  console.log("transformedData: ", data)
+  console.log(data)
+}
+
+//Load the data and return a promise which resolves with said data
+function loadData(url, query){
+  return cleanedDataset
+    .then(data => config.results.bindings)
+}
+
+//Nest the data per country
+function transformData(source){
+  let transformed =  d3.nest()
+		.key(function(d) { return d.landLabel; })
+		.entries(source);
+  transformed.forEach(country => {
+    country.amount = country.values.length
+  })
+  return transformed
+}
+
+//This function gets the nested value out of the object in each property
+// in our data
+function cleanData(row){
+   let result = {}
+    Object.entries(row)
+    	.forEach(([key, propValue]) => {
+				result[key] = propValue.value
+  	})
+   return result
+}
+
+
+
+
+
+
+
+
+
+
+
+
+// CODE VOOR DE KAART STAAT HIERONDER
+
+// import { select, json, geoPath, geoNaturalEarth1 } from 'd3';
+// import { feature } from 'topojson';
+//
+// const query = `PREFIX wgs84: <http://www.w3.org/2003/01/geo/wgs84_pos#>
+// PREFIX geo: <http://www.opengis.net/ont/geosparql#>
+// PREFIX gn: <http://www.geonames.org/ontology#>
+// PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+// PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
+// PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+// PREFIX dc: <http://purl.org/dc/elements/1.1/>
+// PREFIX dct: <http://purl.org/dc/terms/>
+// PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+// PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+// PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+//
+// # let op: geeft aantal van unieke combinaties van ?date en ?landLabel
+// SELECT ?landLabel ?lat ?long ?date (COUNT(?cho) AS ?choCount) WHERE {
+//    ?cho dct:created ?date;
+//         dct:spatial ?plaats .
+//  # We willen geen datums die de string [NI] bevatten
+//    FILTER (!REGEX(?date, "[NI]")) . #zorgt ervoor dat de string "NI" niet wordt meegenomen
+//  # geef het label van het land waar de plaats ligt en de lat/long van het land
+//    ?plaats skos:exactMatch/gn:parentCountry ?land .
+//    ?land wgs84:lat ?lat .
+//    ?land wgs84:long ?long .
+//    ?land gn:name ?landLabel .
+// } GROUP BY ?date ?landLabel ?lat ?long
+// ORDER BY DESC(?choCount)`
+// //Please use your own endpoint when using this
+// const endpoint = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-14/sparql"
+//
+// const svg = select('svg')
+// const circleDelay = 10
+// const circleSize = 8
+// const projection = geoNaturalEarth1()
+// const pathGenerator = geoPath().projection(projection)
+//
+// setupMap()
+// drawMap()
+// plotLocations()
+//
+// function setupMap(){
+//   svg
+//     .append('path')
+//     .attr('class', 'sphere')
+//     .attr('d', pathGenerator({ type: 'Sphere' }))
+// }
+//
+// function drawMap() {
+//   d3.json('https://unpkg.com/world-atlas@1.1.4/world/110m.json').then(data => {
+//     const countries = feature(data, data.objects.countries);
+//     svg
+//       .selectAll('path')
+//       .data(countries.features)
+//       .enter()
+//       .append('path')
+//       .attr('class', 'country')
+//       .attr('d', pathGenerator)
+//   })
+// }
+//
+// function plotLocations() {
+//   fetch(endpoint +"?query="+ encodeURIComponent(query) + "&format=json")
+//     .then(data => data.json())
+//   	.then(json => json.results.bindings)
+//     .then(results => {
+//     //TODO: clean up results in separate function
+//     	results.forEach(result => {
+//         result.lat = Number(result.lat.value)
+//         result.long = Number(result.long.value)
+//       })
+//     	console.log(results)
+//
+//     svg
+//         .selectAll('circle')
+//         .data(results)
+//         .enter()
+//         .append('circle')
+//         .attr('class', 'circles')
+//         .attr('cx', function(d) {
+//           return projection([d.long, d.lat])[0]
+//         })
+//         .attr('cy', function(d) {
+//           return projection([d.long, d.lat])[1]
+//         })
+//         .attr('r', '0px')
+//     		//Opacity is quite heavy on the rendering process so I've turned it off
+//     		//.attr('opacity', .5)
+//         .transition()
+//     				.delay(function(d, i) { return i * circleDelay; })
+//             .duration(1500)
+//             .ease(d3.easeBounce)
+//             .attr('r', circleSize+'px')
+//   })
+// }
